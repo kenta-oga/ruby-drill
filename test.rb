7b23b5efc30d1b108d82e4b3b64ec152d6aa8c44
kenta-oga <kenta555phi@gmail.com>
2021-03-04 23:53:33 +0900

    カロリー計算問題

--- test.rb
+++ test.rb
@@ -1,54 +1,53 @@
-def get_days(year, month)
-  days = 0
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  if month == 2
-    if year % 4 == 0
-      if year % 100 == 0 && year % 400 != 0
-        days = 28
-      else
-        days = 29
-      end
-    else
-      days = 28
-    end
-  else
-    days = month_days[month - 1]
+class Food
+  @@foods = []
+
+  def self.input
+    puts "料理名を入力してください:"
+    name = gets.chomp
+    puts "カロリーを入力してください:"
+    calory = gets.to_i
+
+    food = Food.new(name, calory)
+    @@foods << food
   end
 
-  return days
+  def self.show_all_calory
+    all_calory = 0
+    line = "-----------------------------"
+    puts line
+
+    @@foods.each do |food|
+      puts "#{food.name}  :#{food.calory}kcal"
+      all_calory = all_calory + food.calory
+    end
+
+    puts line
+    puts "カロリー合計  :#{all_calory}kcal"
+  end
+
+  def initialize(name, calory)
+    @name = name
+    @calory = calory
+  end
+
+  def name
+    return @name
+  end
+
+  def calory
+    return @calory
+  end
 end
 
-def get_week(year, month, day)
-  weeks = ["月", "火", "水", "木", "金", "土", "日"]
-  days = 0
+while true do
+  puts "[0]:カロリーを入力する"
+  puts "[1]:カロリーの合計を見る"
+  input = gets.to_i
 
-  year_index = year - 1
-  while year_index > 0 do
-    if get_days(year_index, 2) == 29
-      days = days + 366
-    else
-      days = days + 365
-    end
-    year_index = year_index - 1
+  if input == 0
+    Food.input           # カロリーの入力
+  elsif input == 1
+    Food.show_all_calory # カロリーの合計を表示
+    exit
   end
-
-  month_index = month - 1
-  while month_index > 0 do
-    month_days = get_days(year, month_index)
-    days = days + month_days
-    month_index = month_index - 1
-  end
-
-  days = days + day # dayを足す
-  return weeks[(days - 1) % 7]
 end
-
-puts "年を入力してください："
-year = gets.to_i
-puts "月を入力してください："
-month = gets.to_i
-puts "日を入力してください："
-day = gets.to_i
-
-week = get_week(year, month, day)
-puts "#{year}年#{month}月#{day}日は#{week}曜日です"
